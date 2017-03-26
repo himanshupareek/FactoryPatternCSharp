3728f857f66cbddae42ee7ee6025d75a9c94e0dc
himanshupareek <himanshupareek66@gmail.com>
2017-03-26 19:31:15 +0530

    First Commit
    This is one simple example of Factory Pattern in C#

---
+++ FactoryPattern/FactoryPattern/ClsFactoryExportCreational.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class ClsFactoryExportCreational
+    {
+        static public IExport GetExport(int exportType)
+        {
+            IExport objExport;
+
+            switch (exportType)
+            {
+                case 1 :
+                    objExport = new ClsExportToExcel();
+                    break;
+                case 2:
+                    objExport = new ClsExportToPdf();
+                    break;
+                default:
+                    objExport = new ClsExportToExcel();
+                    break;
+            }
+
+            return objExport;
+        }
+    }
+}
---
+++ FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        //solving mostly two problems :
+        //1. Lost of scattered new keyword
+        //2. Client is not aware of any invoice types.
+        static void Main(string[] args)
+        {
+            try
+            {
+                int exportType = 0;
+                IExport objExport;
+
+                Console.WriteLine("Enter the export type: ");
+
+                exportType = Convert.ToInt32(Console.ReadLine());
+
+                objExport = ClsFactoryExportCreational.GetExport(exportType);
+
+                objExport.Export();
+
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Either you have put a value which is not convertable into integer or there might be some other issue.");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
